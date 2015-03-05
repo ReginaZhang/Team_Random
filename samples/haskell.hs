1ad5cd3709989132aead35c0862ce34e61ae0239
logicchains <jonathan.t.barnard@gmail.com>
2015-03-05 22:37:18 +1100

    Added Haskell sample

---
+++ samples/haskell.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Web.Scotty
+
+import Data.Text.Lazy as T
+import Control.Monad.IO.Class
+import System.Random as R
+
+main = scotty 8081 $ do
+  get "/" $ do
+    rnd <- liftIO . getStdRandom $ randomR (1,10000 :: Int)
+    html $ T.concat ["Hello Team ", T.pack $ show rnd]
