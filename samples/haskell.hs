266a4c707558804d0895e1c7fd0f0ed4a70fb969
logicchains <jonathan.t.barnard@gmail.com>
2015-03-06 15:25:31 +1100

    Added Go sample

--- samples/haskell.hs
+++ samples/haskell.hs
@@ -8,5 +8,5 @@ import System.Random as R
 
 main = scotty 8081 $ do
   get "/" $ do
-    rnd <- liftIO . getStdRandom $ randomR (1,10000 :: Int)
-    html $ T.concat ["Hello Team ", T.pack $ show rnd]
+    rnd <- liftIO $ getStdRandom $ randomR (1,10000 :: Int)
+    html $ T.pack $ "Hello Team " ++  (show rnd)
