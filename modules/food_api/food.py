e809db0f6572fd40870585eadcdcf636ea3d9c9e
ReginaZhang <405714219@qq.com>
2015-03-11 14:31:45 +1100

    Directory for keeping modules created

---
+++ modules/food_api/food.py
@@ -0,0 +1,13 @@
+"""
+	Module Name: Food API
+	
+	Author: Regina Zhang
+	
+	Description:
+		Just exist for creating the folder at the moment :p
+		
+	Module created on 11/03/2015
+	
+	Modification logs:
+	
+"""
