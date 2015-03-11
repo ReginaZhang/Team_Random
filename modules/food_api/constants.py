b99b64052f99dd1587513ea997254b5a25e1ea77
ReginaZhang <405714219@qq.com>
2015-03-11 15:36:25 +1100

    File for storing constants needed for food api created

---
+++ modules/food_api/constants.py
@@ -0,0 +1,15 @@
+"""
+	Module name: Constants
+	
+	Author: Regina Zhang
+	
+	Description:
+		A python file for storing constants needed.
+		
+	Module created on 11/03/2015
+	
+	Modification logs:
+	
+"""
+
+common = {"api_key" : "AB66LrhBWbvW9wEXTKCwuVkfclEnOxoWZinWjIY7"}
