4c373d4029787c5269aba124dff537bf5d8c24de
ReginaZhang <405714219@qq.com>
2015-03-12 14:05:04 +1100

    the output of get_list function is finally human-readable now >.<

--- modules/food_api/food.py
+++ modules/food_api/food.py
@@ -9,14 +9,18 @@
 	Module created on 11/03/2015
 	
 	Modification logs:
+		12/03/2015: get_list function added
 	
 """
 
 import requests
+import json
 import unicodedata
 from constants import common
 from constants import list
 import cherrypy
+
+debug = 0
 
 class FoodAPI:
 	
@@ -28,9 +32,15 @@ class FoodAPI:
 		self.params = {"api_key":common["api_key"], "max":max, "offset":offset, "sort":sort}
 		self.url = list["url"]
 		r = requests.get(self.url, params = self.params)
-		return r.text
+		cherrypy.response.headers["content-type"] = "application/json"
+		return json.dumps(r.json())
 	get_list.exposed = True
-	
-cherrypy.config.update({"server.socket_port": 1234})
-#cherrypy.config.update({"server.socket_host":"45.56.85.191"})
-cherrypy.quickstart(FoodAPI())
+
+if __name__ == '__main__':
+	if not debug:
+		cherrypy.config.update({"server.socket_port": 1234})
+		#cherrypy.config.update({"server.socket_host":"45.56.85.191"})
+		cherrypy.quickstart(FoodAPI())
+	else:
+		a = FoodAPI()
+		a.get_list()
