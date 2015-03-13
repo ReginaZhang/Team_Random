6461af580fc3de5e191f3a3ae6043e30e1c39e40
Bingfeng Liu <liubingfeng93@gmail.com>
2015-03-13 15:04:12 +1100

    drafting index.html

---
+++ modules/web_page/js/style.js
@@ -0,0 +1,12 @@
+/**
+ * Created by liubingfeng on 13/03/15.
+ */
+function test()
+{
+    document.body.innerHTML="<h1>Testing JS</h1>";
+}
+
+function test_jq()
+{
+    $("body").html("<h1>Testing JQ</h1>")
+}
