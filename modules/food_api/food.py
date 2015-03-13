4a49eeacadefc5b238a92c0622ecc36a27b24567
ReginaZhang <405714219@qq.com>
2015-03-13 14:44:48 +1100

    Doc string added to the functions

--- modules/food_api/food.py
+++ modules/food_api/food.py
@@ -30,6 +30,21 @@ class FoodAPI:
 		self.url = None
 	
 	def get_list(self, lt = 'f', max = 10, offset = 0, sort = "n"):
+		"""
+			get_list(lt, max, offset, sort)
+			
+			@params:
+				lt: List type. food "f", all nutrients "n", pecial nutrients "ns",
+					standard release nutrients only "nr", food group "g".
+					Default = "f"
+				max: The maximum number of items returned. Default = 10
+				offset: The offset of the first item in the result. Default = 0
+				sort: Sort the result by food name or id "n". Default = "n"
+				
+			@return:
+				JSON object of the list of items requested.
+				Including offset, id and food name.
+		"""
 		self.params = {"api_key":common["api_key"], "max":max, "offset":offset, "sort":sort}
 		self.url = url["list"]
 		r = requests.get(self.url, params = self.params)
@@ -38,6 +53,20 @@ class FoodAPI:
 	get_list.exposed = True
 	
 	def search_food(self, term = "", max = 10, offset = 0, sort = "r"):
+		"""
+			search_food(term, max, offset, sort)
+			
+			@params:
+				term: Search term. Default = ""
+				max: The maximum number of items returned. Default = 10
+				offset: The offset of the first item in the result. Default = 0
+				sort: Sort the result by food name("n");
+					sort by search relevance("r").
+					Default = "r"
+					
+			@return:
+				JSON object of the list of items requested.
+		"""
 		self.params = {"api_key":common["api_key"], "q": term, "max":max, "offset":offset, "sort":sort}
 		self.url = url["search"]
 		r = requests.get(self.url, params = self.params)
