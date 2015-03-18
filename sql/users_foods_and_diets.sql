53e8247bf64d4cdb63987cf87ad6fa86cd347a7f
Jonathan Barnard <jonathan.t.barnard@gmail.com>
2015-03-18 12:56:37 +1100

    Made username not an int

--- sql/users_foods_and_diets.sql
+++ sql/users_foods_and_diets.sql
@@ -8,7 +8,7 @@ create table if not exists Address(
 
 create table if not exists User(
   UserId int NOT NULL AUTO_INCREMENT,
-  UserName int NOT NULL,
+  UserName varchar(255) NOT NULL,
   AddressId int NOT NULL,
   Email varchar(255) NOT NULL,
   PRIMARY KEY (UserId),
