ceda61d8751041482e332b6458c6b70cbb607c48
Bingfeng Liu <liubingfeng93@gmail.com>
2015-03-23 21:41:25 +1100

    carousel nearly working properly

--- modules/web_page/js/carousel.js
+++ modules/web_page/js/carousel.js
@@ -43,7 +43,84 @@ function return_image_position_0(obj_collection) {
     return index;
 }
 function carousel_moving(left_right) {
-    var i, index;
+    var i, index, shift_index, obj_collection;
+    obj_collection = document.getElementsByClassName("balloon");
+    index = return_image_position_0(obj_collection);
     /*current focused item's index, i.e. middle one*/
+    shift_index = index + parseInt(left_right);
+    var obj_collection_outer_div = document.getElementById("balloon_section");
+    var obj_collection_outer_div_width = obj_collection_outer_div.offsetWidth;
+    var obj_one_width = obj_collection[0].offsetWidth;
+    if (shift_index < 0 || shift_index >= obj_collection.length) {
+        return;
+    }
+    /*-1 move left ..*/
+    if (parseInt(left_right) == -1) {
+        /*last focused one will be on right*/
+        obj_collection[index].setAttribute("image-position", "1");
+        obj_collection[shift_index].setAttribute("image-position", "0");
+        /*
+        obj_collection[index].style.webkitTransform="perspective( 400px ) rotateY(-45deg) translateZ( -50px )";
+        obj_collection[index].style.MozTransform="perspective( 400px ) rotateY(-45deg) translateZ( -50px )";
+        obj_collection[index].style.transform="perspective( 400px ) rotateY(-45deg) translateZ( -50px )";*/
+        /*
+        obj_collection[shift_index].style.MozTransform="perspective( 400px ) translateZ( 50px )";
+        obj_collection[shift_index].style.Transform="perspective( 400px ) translateZ( 50px )";
+        obj_collection[shift_index].style.transform="perspective( 400px ) translateZ( 50px )";
+         */
+        set_carousel_transform(obj_collection[index], (obj_collection_outer_div_width - obj_one_width) - 50 - 10 * (obj_collection.length - index) + "px", "400px", "-45deg", "-50px", "" + obj_collection.length - index);
+        set_carousel_transform(obj_collection[shift_index], (obj_collection_outer_div_width / 2) - (obj_one_width / 2) + "px", "400px", "0deg", "25px", shift_index + "");
+    }
+    else {
+        obj_collection[index].setAttribute("image-position", "-1");
+        obj_collection[shift_index].setAttribute("image-position", "0");
+        /*obj_collection[index].style.webkitTransform="perspective( 400px ) rotateY(45deg) translateZ( -50px )";
+        obj_collection[index].style.MozTransform="perspective( 400px ) rotateY(45deg) translateZ( -50px )";
+        obj_collection[index].style.transform="perspective( 400px ) rotateY(45deg) translateZ( -50px )";
+        obj_collection[shift_index].style.MozTransform="perspective( 400px ) translateZ( 50px )";
+        obj_collection[shift_index].style.Transform="perspective( 400px ) translateZ( 50px )";
+        obj_collection[shift_index].style.transform="perspective( 400px ) translateZ( 50px )";*/
+        set_carousel_transform(obj_collection[index], 50 + 10 * (obj_collection.length - index) + "px", "400px", "45deg", "-50px", "" + index);
+        set_carousel_transform(obj_collection[shift_index], (obj_collection_outer_div_width / 2) - (obj_one_width / 2) + "px", "400px", "0deg", "25px", shift_index + "");
+    }
+    /*
+    console.log(left_right+" index : "+index+" shift index "+shift_index);
+    console.log("last focused image-position"+obj_collection[index].getAttribute("image-position"));
+    console.log("new/shift focused image-position"+obj_collection[shift_index].getAttribute("image-position"));*/
+}
+function carousel_initialize() {
+    var obj_collection = document.getElementsByClassName("balloon");
+    var obj_collection_length = obj_collection.length;
+    var obj_collection_outer_div = document.getElementById("balloon_section");
+    var obj_collection_outer_div_width = obj_collection_outer_div.offsetWidth;
+    var obj_one_width = obj_collection[0].offsetWidth;
+    console.log(document.getElementById("balloon_section").style.width);
+    var i = 0;
+    var mid_index = Math.floor(obj_collection_length / 2);
+    console.log("mid index " + mid_index);
+    var one_element;
+    for (i = 0; i < mid_index; i++) {
+        //all things on left
+        one_element = obj_collection[i];
+        one_element.setAttribute("image-position", "-1");
+        set_carousel_transform(one_element, 50 + 10 * i + "px", "400px", "45deg", "-50px", "" + i);
+    }
+    one_element = obj_collection[mid_index];
+    set_carousel_transform(one_element, (obj_collection_outer_div_width / 2) - (obj_one_width / 2) + "px", "400px", "0deg", "25px", "" + mid_index);
+    for (i = mid_index + 1; i < obj_collection_length; i++) {
+        one_element = obj_collection[i];
+        one_element.setAttribute("image-position", "-1");
+        set_carousel_transform(one_element, (obj_collection_outer_div_width - obj_one_width) - 50 - 10 * (obj_collection_length - i) + "px", "400px", "-45deg", "-50px", "" + (obj_collection_length - i));
+    }
+}
+function set_carousel_transform(one_element, left, perspective, degree, translateZ, z_index) {
+    one_element.style.zIndex = z_index;
+    one_element.style.left = left;
+    console.log("left:" + left);
+    one_element.style.webkitTransform = "perspective(" + perspective + ") rotateY(" + degree + ") translateZ(" + translateZ + ")";
+    one_element.style.MozTransform = "perspective(" + perspective + ") rotateY(" + degree + ") translateZ(" + translateZ + ")";
+    one_element.style.transform = "perspective(" + perspective + ") rotateY(" + degree + ") translateZ(" + translateZ + ")";
+    //one_element.style.MozTransform="perspective( 400px ) rotateY(45deg) translateZ( -50px )";
+    //one_element.style.transform="perspective( 400px ) rotateY(45deg) translateZ( -50px )";
 }
 //# sourceMappingURL=carousel.js.map
