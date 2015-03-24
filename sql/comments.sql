4f3f97b402e9dad74878060ad3a8baa9297683a0
jonathan <jonathan.t..barnard@gmail.com>
2015-03-24 21:24:08 +1100

    Made commentid autoincrement

--- sql/comments.sql
+++ sql/comments.sql
@@ -1,8 +1,8 @@
 use HealthDB;
 
 
-create table if not exists Comment (
-       CommentId int NOT NULL,
+create table if not exist Comment (
+       CommentId int NOT NULL AUTO_INCREMENT,
        ParentId int,
        QuestionId int NOT NULL,
        Text text NOT NULL,
