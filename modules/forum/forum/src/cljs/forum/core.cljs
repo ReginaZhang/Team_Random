a2e9e3c3122d9f8f26f0c0933c5ed6355ace6211
jonathan <jonathan.t..barnard@gmail.com>
2015-03-29 22:47:03 +1100

    preparing for ocmments

--- modules/forum/forum/src/cljs/forum/core.cljs
+++ modules/forum/forum/src/cljs/forum/core.cljs
@@ -4,6 +4,18 @@
 (enable-console-print!)
 
 (defn hello [name]
-  [:div "hello " name])
+  [:div "hello " name
+   [expandable-view]])
+
+(defn expandable-view []
+  (let [expanded (reagent/atom true)]
+    (fn []
+      [:div.expandable
+       [:div.header {:on-click #(swap! expanded not)}
+        "Click me to expand and collapse"]
+       (if @expanded
+         (for [comment ["cat" "dog" "mouse"]]
+           [:div.body comment]))])))
+;         [:div.body "I am the body"])])))
 
 (reagent/render [hello "Nima"] (.-body js/document))
