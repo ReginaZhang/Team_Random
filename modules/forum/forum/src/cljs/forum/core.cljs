bce8bc0eee5abfa1592dc44c3a9dbd2c67af0e80
jonathan <jonathan.t..barnard@gmail.com>
2015-04-06 22:47:32 +1000

    Display new comments after add

--- modules/forum/forum/src/cljs/forum/core.cljs
+++ modules/forum/forum/src/cljs/forum/core.cljs
@@ -22,21 +22,23 @@
 (defn request-child-comments
   "Make an asynchronous request to get child comments and store them
   in the given atom, updating the cache to contain the results"
-  [comment-id store cache]
+  [comment-id cache]
   (backend-request "/child_comments"
                    {:parent_id comment-id}
-                   (fn [[ok response]] (if ok (do
-                                                (reset! store {:children response})
-                                                (swap! cache assoc comment-id {:children response}))
+                   (fn [[ok response]] (if ok (let [store (:store (get @cache comment-id))
+                                                    new-val {:children response :store store }]
+                                                (swap! cache assoc comment-id new-val)
+                                                (reset! store {:children response}))
                                            (.error js/console (str response))))))
 
 (defn add-comment
   "Make a backend request to add a comment"
-  [question-id parent-id user-id parent-box-toggle text error-store]
+  [question-id parent-id user-id parent-box-toggle text error-store update-callback]
   (backend-request "/add_comment"
                    {:question_id question-id :parent_id parent-id :user_id user-id :text text}
                    (fn [[ok response]] (if ok 
-                                         (reset! parent-box-toggle false)
+                                         (do (reset! parent-box-toggle false)
+                                             (update-callback))
                                          (reset! error-store (str response))))))
 
 (defn start-resource-provider
@@ -46,21 +48,38 @@
   containing that comment's child comments."
   [request-chan]
   (let [c request-chan        
-        cache (re/atom {})] ;a map of post ids to children
+        cache (re/atom {})] ;a map of post ids to {:children :store} maps
     (go
       (while true
         (let [req (<! c)
-              post-id (:comment-id req)
-              children-store (:atom req)
-              cached-children (get @cache post-id)]
-          (if cached-children
-            (reset! children-store cached-children)
-            (request-child-comments post-id children-store cache)))))))
+              req-type (:type req)]
+          (case req-type
+            :children-request
+            (let
+                [post-id (:comment-id req)
+                 children-store (:atom req)
+                 cached-val (get @cache post-id)]
+              (if cached-val
+                ;;If cached val exists, use it
+                (reset! children-store cached-val)
+                
+                ;;Otherwise add store to cache and request updating it
+                (do
+                  (swap! cache assoc post-id {:children nil :store children-store})
+                  (request-child-comments post-id cache))))
+            
+            :update-children
+            (let
+                [post-id (:comment-id req)
+                 children-store (:atom req)
+                 cached-val (get @cache post-id)]
+              (when cached-val
+                  (request-child-comments post-id cache)))))))))
 
 (def request-chan (chan))
 (start-resource-provider request-chan)
 
-(defn comment-entry-box [{:keys [parent-id user-id question-id parent-box-toggle error-store]}]
+(defn comment-entry-box [{:keys [parent-id user-id question-id parent-box-toggle error-store update-callback]}]
   "Render a comment entry box. Disables the parent box toggle upon successful;y adding
    a comment, and updates parent error"
   (let [txt (re/atom "")]
@@ -70,7 +89,7 @@
                 :placeholder "Enter a comment..."
                 :value @txt
                 :on-change #(reset! txt (-> % .-target .-value))}]
-       [:button {:on-click #(add-comment question-id parent-id user-id parent-box-toggle @txt error-store)}
+       [:button {:on-click #(add-comment question-id parent-id user-id parent-box-toggle @txt error-store update-callback)}
         "Submit"]])))
 
 (defn display-comment [req-c {:keys [userid text commentid questionid]}]
@@ -78,7 +97,8 @@
         child-comment-atom (re/atom {})
         showing-comment-entry (re/atom false)
         error-atom (re/atom "")
-        children-req {:comment-id commentid :atom child-comment-atom}]
+        children-req {:type :children-request :comment-id commentid :atom child-comment-atom}
+        children-update-callback (fn [] (go (>! req-c {:type :update-children :comment-id commentid})))]
     (fn []
       [:div.comment-region
        [:div.comment-text (str "Comment by user id: " userid " with comment id: " commentid)]
@@ -89,19 +109,14 @@
          [:div.comment-entry-box-toggle {:on-click #(swap! showing-comment-entry not)}
           (if @showing-comment-entry "Abort comment" "Enter Comment")])
        (when @showing-comment-entry [comment-entry-box {:parent-id commentid :user-id userid :question-id questionid
-                                                        :parent-box-toggle showing-comment-entry
-                                                        :error-store error-atom}])
+                                                        :parent-box-toggle showing-comment-entry :error-store error-atom
+                                                        :update-callback children-update-callback}])
        (when (not= @error-atom "") [:div.error-text @error-atom])
        (when @expanded
          (go (>! req-c children-req))
          (for [child-comment (:children @child-comment-atom)]                                       
            ^{:key (:commentid child-comment)}
            [display-comment req-c (assoc child-comment :questionid questionid)]))])))
-
-
-(defn hello [name]
-  [:div "hello " name
-   [display-comment request-chan]])
 
 (re/render [display-comment request-chan {:userid 0 :text "test comment" :commentid 0 :questionid 0}] (.-body js/document))
 
