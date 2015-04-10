51c3beef4fb26a425e67db03b60aba4bb8305ead
logicchains <jonathan.t.barnard@gmail.com>
2015-04-10 21:13:38 +1000

    Added sql for flags

--- sql/comments.sql
+++ sql/comments.sql
@@ -1,11 +1,30 @@
 use HealthDB;
 
 
-create table Comment (
+create table if not exists Comment(
        CommentId int NOT NULL AUTO_INCREMENT,
        ParentId int,
        QuestionId int NOT NULL,
        Text text NOT NULL,
        UserId int NOT NULL,
        PRIMARY KEY (CommentId)
+       )ENGINE=INNODB;
+
+create table if not exists FlagType(
+       FlagId int NOT NULL AUTO_INCREMENT,
+       FlagName VARCHAR(255),
+       PRIMARY KEY (FlagId)
+       )ENGINE=INNODB;
+
+create table if not exists CommentFlag(
+       FlagId int NOT NULL,
+       CommentId int NOT NULL,       
+       FOREIGN KEY (FlagId) 
+              REFERENCES FlagType(FlagId)
+	      ON UPDATE CASCADE
+    	      ON DELETE RESTRICT,
+       FOREIGN KEY (CommentId) 
+              REFERENCES Comment(CommentId)
+	      ON UPDATE CASCADE
+    	      ON DELETE RESTRICT
        )ENGINE=INNODB;
