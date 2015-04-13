068f4e0717c1687cb261d78409ae566744491574
Bingfeng Liu <liubingfeng93@gmail.com>
2015-04-13 12:18:23 +1000

    prototype 1

---
+++ modules/prototype_1/page_host.py
@@ -0,0 +1,33 @@
+import cherrypy
+import os,os.path
+
+path=os.path.abspath(os.path.dirname('index.html'))
+
+class HealthOverflow(object):
+    @cherrypy.expose
+    def index(self):
+        return file('index.html')
+
+conf={
+        '/css':
+        { 'tools.staticdir.on':True,
+          'tools.staticdir.dir': os.path.join(path,'css')
+        }
+        ,
+        '/js':
+        { 'tools.staticdir.on':True,
+          'tools.staticdir.dir': os.path.join(path,'js')
+        }
+        ,
+        '/asserts':
+        { 'tools.staticdir.on':True,
+          'tools.staticdir.dir': os.path.join(path,'asserts')
+        },
+        '/':
+        { 'tools.staticdir.on':True,
+          'tools.staticdir.dir': os.path.join(path)
+        }
+
+    }
+
+cherrypy.quickstart(HealthOverflow(), '/', conf)
