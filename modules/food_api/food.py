7f645d8ecff4c124eaa6d8d67ac6d766895d49e7
ReginaZhang <405714219@qq.com>
2015-05-16 18:20:12 +1000

    ..

--- modules/food_api/food.py
+++ modules/food_api/food.py
@@ -114,7 +114,7 @@ class FoodAPI:
 		if ((dboffset == 0) and (apioffset == 0)) or ((dboffset != 0) and (apioffset == 0)):
 			result = self.db_search(term, max, dboffset)
 			length = len(result["items"])
-			print "db: " + length
+			print "db: " + str(length)
 			if length < max:
 				dboffset = 0
 				left = max-length
@@ -125,7 +125,7 @@ class FoodAPI:
 		else:
 			result = self.api_search(term, max, apioffset)
 			print max
-			print "api: " + len(result["items"])
+			print "api: " + str(len(result["items"]))
 			apioffset += len(result["items"])
 		result["dboffset"] = dboffset
 		result["apioffset"] = apioffset
